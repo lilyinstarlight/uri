92a57e146de4b11a5bd5bcf3baf5194bf333179e
Foster McLane <fkmclane@gmail.com>
2017-07-23 16:25:36 -0400

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+import os
+import re
 
-from uri import name, version
+from setuptools import setup, find_packages
+
+
+name = None
+version = None
+
+
+def find(haystack, *needles):
+    regexes = [(index, re.compile("^{}\s*=\s*'([^']*)'$".format(needle))) for index, needle in enumerate(needles)]
+    values = ['' for needle in needles]
+
+    for line in haystack:
+        if len(regexes) == 0:
+            break
+
+        for rindex, (vindex, regex) in enumerate(regexes):
+            match = regex.match(line)
+            if match:
+                values[vindex] = match.groups()[0]
+                del regexes[rindex]
+                break
+
+    return values
+
+
+with open(os.path.join(os.path.dirname(__file__), 'uri', '__init__.py'), 'r') as uri:
+    name, version = find(uri, 'name', 'version')
 
 
 setup(
@@ -11,6 +38,8 @@ setup(
     license='MIT',
     author='Foster McLane',
     author_email='fkmclane@gmail.com',
-    packages=['uri'],
-    package_data={'uri': ['html/*.*']},
+    install_requires=[],
+    packages=find_packages(),
+    package_data={'': ['html/*.*', 'res/*.*']},
+    entry_points = {'console_scripts': ['uri = uri.main']},
 )
