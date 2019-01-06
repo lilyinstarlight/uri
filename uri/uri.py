7daf4616be9997bf951d75c520637625a0d2eb7c
Foster McLane <fkmclane@gmail.com>
2019-01-05 19:07:56 -0500

    avoid useless and racey check for existing alias

--- uri/uri.py
+++ uri/uri.py
@@ -35,21 +35,6 @@ def put(alias, location):
     else:
         conn = http.client.HTTPConnection(config.store)
 
-    # request given alias
-    conn.request('HEAD', config.store_endpoint + 'store/uri/' + alias)
-
-    # get response
-    response = conn.getresponse()
-    response.read()
-
-    # check for existing alias
-    if response.status == 200:
-        raise KeyError()
-    elif response.status == 400:
-        raise NameError()
-    elif response.status != 404:
-        raise ValueError()
-
     # determine if this is a put or a post
     if alias:
         method = 'PUT'
@@ -65,8 +50,10 @@ def put(alias, location):
     # load data response
     data = json.loads(response.read().decode('utf-8'))
 
-    # note bad requests
-    if response.status == 404:
+    # note bad requests - existing alias, bad name, and unknown error
+    if response.status == 403:
+        raise KeyError()
+    elif response.status == 404:
         raise NameError()
     elif response.status != 201:
         raise ValueError()
